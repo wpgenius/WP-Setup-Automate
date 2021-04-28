dba75d452fe1b8ed81e53a63923046a723e620fe
sohamjoshi8275 <74714812+sohamjoshi8275@users.noreply.github.com>
2021-04-28 18:20:50 +0530

    add wpauto.sh

---
+++ wpauto.sh
@@ -0,0 +1,10 @@
+echo "Enter folder name : "
+read foldername
+mkdir -p ~/public_html/$foldername && cd ~/public_html/$foldername
+wp core download 
+wp config create --force=y --prompt=dbname,dbuser,dbpass,dbprefix
+wp core install --prompt
+wp plugin delete hello
+wp theme install astra --activate
+wp theme delete twentynineteen twentytwenty twentytwentyone
+wp plugin install elementor contact-form-7 wordpress-seo --activate
