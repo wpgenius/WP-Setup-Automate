96226a7e978a5b1fc6cfd9dbba00d86898d8b7c0
soham joshi <soham@wpgenius.in>
2021-04-28 18:36:04 +0530

    remove plugin: akismet

--- wpauto.sh
+++ wpauto.sh
@@ -4,7 +4,7 @@ mkdir -p ~/public_html/$foldername && cd ~/public_html/$foldername
 wp core download 
 wp config create --force=y --prompt=dbname,dbuser,dbpass,dbprefix
 wp core install --prompt
-wp plugin delete hello
+wp plugin delete hello akismet
 wp theme install astra --activate
 wp theme delete twentynineteen twentytwenty twentytwentyone
 wp plugin install elementor contact-form-7 wordpress-seo --activate
