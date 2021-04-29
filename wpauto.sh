893c94f290b38a31b7a80d376fd8cf839bb497e9
soham joshi <soham@wpgenius.in>
2021-04-29 11:07:08 +0530

    Added new options for change settings.

--- wpauto.sh
+++ wpauto.sh
@@ -3,8 +3,19 @@ read foldername
 mkdir -p ~/public_html/$foldername && cd ~/public_html/$foldername
 wp core download 
 wp config create --force=y --prompt=dbname,dbuser,dbpass,dbprefix
-wp core install --prompt
+wp core install --admin_user=makarand --admin_email=mane.makarand@gmail.com --skip-email=n --prompt
 wp plugin delete hello akismet
 wp theme install astra --activate
 wp theme delete twentynineteen twentytwenty twentytwentyone
 wp plugin install elementor contact-form-7 wordpress-seo --activate
+wp option update blogdescription ""
+wp option update timezone_string "Asia/Kolkata"
+wp option update blog_public "yes"
+wp option update default_pingback_flag "no"
+wp option update default_ping_status "no"
+wp option update default_comment_status "no"
+wp option update comment_registration "yes"
+wp option update comment_moderation "yes"
+wp option update permalink_structure '/%postname%/'
+wp post delete 1
+wp user create pooja pooja@wpgenius.in --role=administrator --user_pass=123 --display_name=pooja --send-email=y --porcelain=y
