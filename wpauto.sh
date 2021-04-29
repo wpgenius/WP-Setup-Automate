935903a0760ca6e39c6a5b8f0641218fb6138e89
Makarand Mane <makarand@wpgenius.in>
2021-04-29 18:55:07 +0530

    Restructure Shell file. Fix few bugs

--- wpauto.sh
+++ wpauto.sh
@@ -1,15 +1,22 @@
+#Create project folder. Here we consider folder name as domain name of project
 echo "Enter folder name : "
 read foldername
 mkdir -p ~/public_html/$foldername && cd ~/public_html/$foldername
+
+#WordPress Installation
 wp core download 
 wp config create --force=y --prompt=dbname,dbuser,dbpass,dbprefix
-wp core install --admin_user=makarand --admin_email=mane.makarand@gmail.com --skip-email=n --prompt
-wp plugin delete hello akismet
-wp theme install astra --activate
-wp scaffold child-theme --author=TeamWPGenius --author_uri=https://wpgenius.in --activate=y --enable-network=y --force=y --prompt
-wp theme delete twentynineteen twentytwenty twentytwentyone
-wp plugin install elementor contact-form-7 --activate
-wp plugin install ga-in wordpress-seo wp https://wpgenius.github.io/WP-Setup-Automate/astra-addon-plugin.zip
+wp core install --admin_user=makarand --admin_email=mane.makarand@gmail.com --url=https://tyche.work/$foldername/ --admin_password= --skip-email=n --prompt
+
+#Install astra theme then create & switch to child theme
+wp theme install astra
+wp scaffold child-theme --author=TeamWPGenius --author_uri=https://wpgenius.in --theme_name=astra --theme_uri=https://$foldername/ --activate=y --enable-network=y --force=y --prompt
+
+#Install necessory plugins
+wp plugin install elementor contact-form-7 https://wpgenius.github.io/WP-Setup-Automate/astra-addon-plugin.zip --activate
+wp plugin install ga-in wordpress-seo
+
+#Update WordPress with default options
 wp option update blogdescription ""
 wp option update timezone_string "Asia/Kolkata"
 wp option update blog_public "yes"
@@ -19,5 +26,11 @@ wp option update default_comment_status "no"
 wp option update comment_registration "yes"
 wp option update comment_moderation "yes"
 wp option update permalink_structure '/%postname%/'
+
+#Delete unwanted data
+wp plugin delete hello akismet
 wp post delete 1
-wp user create pooja pooja@wpgenius.in --role=administrator --user_pass= --display_name=pooja --send-email=y --porcelain=y
+wp theme delete twentynineteen twentytwenty twentytwentyone
+
+#Create additional users
+wp user create pooja pooja@wpgenius.in --role=administrator --user_pass= --display_name=pooja --send-email=y
