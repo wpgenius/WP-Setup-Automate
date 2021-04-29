ebe03d07fc591fc5891f151cb98bff550c035760
Makarand Mane <makarand@wpgenius.in>
2021-04-29 19:06:12 +0530

    Fix URL bug & fix parent theme

--- wpauto.sh
+++ wpauto.sh
@@ -6,11 +6,11 @@ mkdir -p ~/public_html/$foldername && cd ~/public_html/$foldername
 #WordPress Installation
 wp core download 
 wp config create --force=y --prompt=dbname,dbuser,dbpass,dbprefix
-wp core install --admin_user=makarand --admin_email=mane.makarand@gmail.com --url=https://tyche.work/$foldername/ --admin_password= --skip-email=n --prompt
+wp core install --admin_user=makarand --admin_email=mane.makarand@gmail.com --url=tyche.work/$foldername --admin_password= --skip-email=n --prompt
 
 #Install astra theme then create & switch to child theme
 wp theme install astra
-wp scaffold child-theme --author=TeamWPGenius --author_uri=https://wpgenius.in --theme_name=astra --theme_uri=https://$foldername/ --activate=y --enable-network=y --force=y --prompt
+wp scaffold child-theme --author=TeamWPGenius --author_uri=https://wpgenius.in --parent_theme=astra --theme_uri=https://$foldername/ --activate=y --enable-network=y --force=y --prompt
 
 #Install necessory plugins
 wp plugin install elementor contact-form-7 https://wpgenius.github.io/WP-Setup-Automate/astra-addon-plugin.zip --activate
