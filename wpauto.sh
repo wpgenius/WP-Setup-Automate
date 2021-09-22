605c9eb8f3142f0bfd4113609ec32906202afa0e
Makarand Mane <makarand@wpgenius.in>
2021-09-23 01:19:03 +0530

    Moved repeating URL characters to one variable.
    Easy to maintain or reuse

--- wpauto.sh
+++ wpauto.sh
@@ -11,15 +11,17 @@ while [[ $foldername = "" ]]; do
 done
 mkdir -p ~/public_html/$foldername && cd ~/public_html/$foldername
 
+URL=https://tyche.work/$foldername/
+
 #WordPress Installation
 wp core download --quiet
 echo -e "${GREEN}Create databse configuration for ${BLUE}$foldername${NC}"
 wp config create --force=y --prompt=dbname,dbuser,dbpass,dbprefix --quiet
-wp core install --url=https://tyche.work/$foldername/ --admin_user=makarand --admin_email=mane.makarand@gmail.com --admin_password=p@55w0rd! --skip-email=n --prompt=title --quiet
+wp core install --url="$URL" --admin_user=makarand --admin_email=mane.makarand@gmail.com --admin_password=p@55w0rd! --skip-email=n --prompt=title --quiet
 #Un-comment below lines if above command asks for URL parameter
-#wp option update siteurl https://tyche.work/$foldername/
-#wp option update home https://tyche.work/$foldername/
-echo -e "${GREEN}WordPress installed & configured : ${BLUE}https://tyche.work/$foldername${NC}"
+#wp option update siteurl "$URL"
+#wp option update home "$URL"
+echo -e "${GREEN}WordPress installed & configured : ${BLUE}$URL ${NC}"
 
 #Install astra theme then create & switch to child theme
 wp theme install astra --quiet
@@ -68,5 +70,5 @@ wp theme delete twentynineteen twentytwenty twentytwentyone --quiet
 echo -e "${GREEN}Create first developer users account on ${BLUE}$foldername${NC}"
 wp user create pooja pooja@wpgenius.in --role=administrator --user_pass= --display_name=pooja --send-email=y  --quiet
 wp user reset-password makarand --skip-email --quiet
-echo -e "${GREEN}Staging setup is ready ${BLUE}https://tyche.work/$foldername/${NC}"
-curl -d "user_login=makarand&amp;redirect_to=&amp;wp-submit=Get New Password" -X POST https://tyche.work/$foldername/wp-login.php?action=lostpassword
+echo -e "${GREEN}Staging setup is ready ${BLUE}$URL ${NC}"
+curl -d "user_login=makarand&amp;redirect_to=&amp;wp-submit=Get New Password" -X POST "$URL"wp-login.php?action=lostpassword
