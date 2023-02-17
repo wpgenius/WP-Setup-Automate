196fcc558fc2180b4e8879e6c243ca05ef7f2ac7
Makarand Mane <mane.makarand@gmail.com>
2023-02-17 20:25:13 +0530

    Fix #12 give choice to admin whether to add files or skip.

--- wpauto.sh
+++ wpauto.sh
@@ -45,6 +45,8 @@ while [[ $dev_name = "" ]]; do
    read dev_name
 done
 
+#Choices
+read -p "Do you wish to add default files to child theme? (Yes/No) " child_theme_default_files_yn
 mkdir -p ~/public_html/$foldername && cd ~/public_html/$foldername
 
 URL=https://tyche.work/$foldername/
@@ -147,5 +149,9 @@ echo -e "${GREEN}Staging setup is ready ${BLUE}$URL ${NC}"
 curl -d "user_login=makarand&amp;redirect_to=&amp;wp-submit=Get New Password" -X POST "$URL"wp-login.php?action=lostpassword
 
 #Add default files to child theme
-cd wp-content/themes/ && wget https://github.com/wpgenius/Astra-Child-Theme/archive/refs/heads/main.zip -q && unzip -q main.zip && rm Astra-Child-Theme-main/style.css main.zip
-mv Astra-Child-Theme-main/* $theme_slug && rm -r Astra-Child-Theme-main
+case $child_theme_default_files_yn in
+    [Yy]* ) 
+    cd wp-content/themes/ && wget https://github.com/wpgenius/Astra-Child-Theme/archive/refs/heads/main.zip -q && unzip -q main.zip && rm Astra-Child-Theme-main/style.css main.zip
+    mv Astra-Child-Theme-main/* $theme_slug && rm -r Astra-Child-Theme-main
+    ;;
+esac
