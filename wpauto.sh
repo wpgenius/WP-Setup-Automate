084097af49d775f2b48df72be384d900dc4e6f3a
sohamjoshi827 <soham@wpgenius.in>
2023-02-17 15:43:25 +0530

    #9 Create some new variables to get input from user before wordpress installation.

--- wpauto.sh
+++ wpauto.sh
@@ -9,15 +9,54 @@ echo -e "${GREEN}Enter folder name :${NC} "
 while [[ $foldername = "" ]]; do
    read foldername
 done
+
+echo -e "${GREEN}Enter slug for child theme :${NC} "
+while [[ $theme_slug = "" ]]; do
+   read theme_slug
+done
+
+echo -e "${GREEN}Enter the site title:${NC} "
+while [[ $TITLE = "" ]]; do
+   read TITLE
+done
+
+echo -e "${GREEN}Enter the database name :${NC} "
+while [[ $dbname = "" ]]; do
+   read dbname
+done
+
+echo -e "${GREEN}Enter the database user name :${NC} "
+while [[ $dbuser = "" ]]; do
+   read dbuser
+done
+
+echo -e "${GREEN}Enter the database user password :${NC} "
+while [[ $dbpass = "" ]]; do
+   read dbpass
+done
+
+echo -e "${GREEN}Enter the database table prefix :${NC} "
+while [[ $dbprefix = "" ]]; do
+   read dbprefix
+done
+
+echo -e "${GREEN}Enter first developer username :${NC} "
+while [[ $dev_name = "" ]]; do
+   read dev_name
+done
+
 mkdir -p ~/public_html/$foldername && cd ~/public_html/$foldername
 
 URL=https://tyche.work/$foldername/
 
 #WordPress Installation
 wp core download --quiet
-echo -e "${GREEN}Create databse configuration for ${BLUE}$foldername${NC}"
-wp config create --force=y --prompt=dbname,dbuser,dbpass,dbprefix --quiet
-wp core install --url="$URL" --admin_user=makarand --admin_email=mane.makarand@gmail.com --admin_password=p@55w0rd! --skip-email=n --prompt=title --quiet
+
+echo -e "${GREEN}Creating databse configuration for ${BLUE}$foldername${NC}"
+wp config create --dbname="$dbname" --dbuser="$dbuser" --dbpass="$dbpass" --dbprefix="$dbprefix" --force=y --quiet
+
+echo -e "${GREEN}Installing WordPress for ${BLUE}$foldername${NC}"
+wp core install --url="$URL" --title="$TITLE" --admin_user=makarand --admin_email=mane.makarand@gmail.com --admin_password=p@55w0rd! --skip-email=n --quiet
 #Un-comment below lines if above command asks for URL parameter
 #wp option update siteurl "$URL"
 #wp option update home "$URL"
@@ -27,8 +66,8 @@ wp config set WP_MEMORY_LIMIT 256M  --quiet
 
 #Install astra theme then create & switch to child theme
 wp theme install astra --quiet
-echo -e "${GREEN}Create child theme for ${BLUE}$foldername${NC}"
-wp scaffold child-theme --author="Team WPGenius" --author_uri=https://wpgenius.in --parent_theme=astra --theme_uri=https://$foldername/ --activate=y --enable-network=y --force=y --prompt --quiet
+echo -e "${GREEN}Creating child theme for ${BLUE}$foldername${NC}"
+wp scaffold child-theme slug=$theme_slug --theme_name="$TITLE theme" --author="Team WPGenius" --author_uri=https://wpgenius.in --parent_theme=astra --theme_uri=https://$foldername/ --activate=y --enable-network=y --force=y --prompt --quiet
 
 #Install necessory plugins
 echo -e "${GREEN}Installing necessory plugin on ${BLUE}$foldername${NC}"
@@ -56,7 +95,6 @@ if [ -r "$uae_key" ]; then
 		wp plugin update ultimate-elementor --quiet
 	fi
 fi
-
 
 #Activate Premium Starter sites
 pst_key=~/.pst-pro
@@ -99,8 +137,8 @@ wp config set WP_ENVIRONMENT_TYPE staging --quiet
 wp config set WP_DISABLE_FATAL_ERROR_HANDLER true --raw --quiet
 
 #Create additional users
-echo -e "${GREEN}Create first developer users account on ${BLUE}$foldername${NC}"
-wp user create snehal snehal@wpgenius.in --role=administrator --user_pass= --display_name=snehal --send-email=y  --quiet
+echo -e "${GREEN}Creating first developer ${BLUE}$dev_name's ${GREEN}account on ${BLUE}$foldername${NC}"
+wp user create $dev_name $dev_name@wpgenius.in --role=administrator --user_pass= --display_name=$dev_name --send-email=y  --quiet
 wp user reset-password makarand --skip-email --quiet
 echo -e "${GREEN}Staging setup is ready ${BLUE}$URL ${NC}"
 curl -d "user_login=makarand&amp;redirect_to=&amp;wp-submit=Get New Password" -X POST "$URL"wp-login.php?action=lostpassword
